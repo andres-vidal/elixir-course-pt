5ee770ac87d66e9ef6d8a5cfa2f1000273771ed2
Freynhel <97458860+Freynhel@users.noreply.github.com>
2023-03-06 01:32:55 -0300

    Implementing "Tarefa" Module

--- tarefas-web/backend/lib/tarefas/tarefa.ex
+++ tarefas-web/backend/lib/tarefas/tarefa.ex
@@ -1,59 +1,65 @@
 defmodule Tarefas.Tarefa do
-  @moduledoc """
-  Módulo com a definição da struct Tarefa e com utilizades para a sua manipulação individual
-  """
+	@moduledoc """
+	Módulo com a definição da struct Tarefa e com utilizades para a sua manipulação individual
+	"""
 
-  alias Tarefas.Tarefa
+	alias Tarefas.Tarefa
 
-  @derive {Jason.Encoder, [:descricao, :estado]}
-  @enforce_keys [:descricao]
-  defstruct id: "", descricao: nil, estado: "sem_completar"
+	@derive {Jason.Encoder, [:descricao, :estado]}
+	@enforce_keys [:descricao]
+	defstruct id: "", descricao: nil, estado: "sem_completar"
 
-  @doc """
-  Recebe uma struct do tipo Tarefa
+	@doc """
+	Recebe uma struct do tipo Tarefa
 
-  Retorna uma struct do tipo Tarefa com os mesmo dados e estado "completada"
-  """
-  def completar(_) do
-  end
+	Retorna uma struct do tipo Tarefa com os mesmo dados e estado "completada"
+	"""
+	def completar(%Tarefa{id: id, descricao: descricao} = tarefa) do
+		%Tarefa{id: id, descricao: descricao, estado: "completada"}
+	end
 
-  @doc """
-  Recebe uma struct do tipo Tarefa
+	@doc """
+	Recebe uma struct do tipo Tarefa
 
-  Retorna uma struct do tipo Tarefa com os mesmo dados e estado "sem_completar"
-  """
-  def reiniciar(_) do
-  end
+	Retorna uma struct do tipo Tarefa com os mesmo dados e estado "sem_completar"
+	"""
+	def reiniciar(%Tarefa{id: id, descricao: descricao} = tarefa) do
+		%Tarefa{id: id, descricao: descricao, estado: "sem_completar"}
+	end
 
-  @doc """
-  Recebe uma struct do tipo Tarefa
+	@doc """
+	Recebe uma struct do tipo Tarefa
 
-  Retorna verdadeiro se seu estado for "completada" ou falso se não
-  """
-  def completada?(_) do
-  end
+	Retorna verdadeiro se seu estado for "completada" ou falso se não
+	"""
+	def completada?(%Tarefa{estado: "completada"}), do: true
+	def completada?(%Tarefa{}), do: false
 
-  @doc """
-  Recebe uma struct do tipo Tarefa
+	@doc """
+	Recebe uma struct do tipo Tarefa
 
-  Codifica a struct numa string com o formato "id,descricao,estado"
-  """
-  def codificar(_) do
-  end
+	Codifica a struct numa string com o formato "id,descricao,estado"
+	"""
+	def codificar(%Tarefa{id: id, descricao: descricao, estado: estado}) do
+		[id, descricao, estado] |> Enum.join(",")
+	end
 
-  @doc """
-  Recebe uma string com o formato "id,descricao,estado"
+	@doc """
+	Recebe uma string com o formato "id,descricao,estado"
 
-  Retorna uma struct do tipo Tarefa com os conteúdos da string
-  """
-  def decodificar(_) do
-  end
+	Retorna uma struct do tipo Tarefa com os conteúdos da string
+	"""
+	def decodificar(str) do
+		[id, descricao, estado] = String.split(str, ",")
+		%Tarefa{id: id, descricao: descricao, estado: estado}
+	end
 
-  @doc """
-  Recebe uma struct do tipo tarefa
+	@doc """
+	Recebe uma struct do tipo tarefa
 
-  Imprime a struct no console
-  """
-  def imprimir(_) do
-  end
+	Imprime a struct no console
+	"""
+	def imprimir(%Tarefa{descricao: descricao}) do
+		IO.puts(descricao)
+	end
 end
