f9ac3e2f3ebcda5ade7489987fbea8f89b5107dc
Freynhel <97458860+Freynhel@users.noreply.github.com>
2023-03-12 13:58:53 -0300

    Implemented ControladorTarefas Module

--- tarefas-web/backend/lib/tarefas_web/controllers/controlador_tarefas.ex
+++ tarefas-web/backend/lib/tarefas_web/controllers/controlador_tarefas.ex
@@ -8,21 +8,45 @@ defmodule TarefasWeb.ControladorTarefas do
 
   @uuid Application.compile_env(:tarefas, :uuid)
 
-  def listar(_, _) do
+  def listar(conn, _parametro) do json(conn, tarefas()) end
+
+  def inserir(conn, %{"descricao" => desc}) do
+    nova_tarefa = %Tarefa{id: @uuid.(), descricao: desc}
+
+    tarefas()
+      |> Tarefas.inserir(nova_tarefa)
+      |> Tarefas.salvar()
+    json(conn, nova_tarefa)
   end
 
-  def inserir(_, _) do
+  def mover(conn, %{"origem" => origem, "destino" => destino}) do
+    tarefas()
+      |>Tarefas.mover(to_int(origem), to_int(destino))
+      |>Tarefas.salvar()
+    json(conn, "")
   end
 
-  def mover(_, _) do
+  def remover(conn, %{"posicao" => posicao}) do
+    tarefas()
+      |> Tarefas.remover(to_int(posicao))
+      |> Tarefas.salvar()
+    json(conn, "")
   end
 
-  def remover(_, _) do
+  def completar(conn, %{"posicao" => posicao}) do
+    tarefas()
+      |> Tarefas.completar(to_int(posicao))
+      |> Tarefas.salvar()
+    json(conn, "")
   end
 
-  def completar(_, _) do
+  def reiniciar(conn, %{"posicao" => posicao}) do
+    tarefas()
+      |> Tarefas.reiniciar(posicao)
+      |> Tarefas.salvar()
+    json(conn, "")
   end
 
-  def reiniciar(_, _) do
-  end
+  defp tarefas() do Tarefas.ler() end
+  defp to_int(str) do String.to_integer(str) end
 end
